c75a11fd7e23641432f86cfda0180b60b3cc732c
darma2006 <lopezsolisdarmasoley@gmail.com>
2025-05-15 14:52:08 -0600

    week 02

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -1,23 +1,38 @@
-// wrapper for querySelector...returns matching element
+
 export function qs(selector, parent = document) {
   return parent.querySelector(selector);
 }
-// or a more concise version if you are into that sort of thing:
-// export const qs = (selector, parent = document) => parent.querySelector(selector);
 
-// retrieve data from localstorage
+
 export function getLocalStorage(key) {
   return JSON.parse(localStorage.getItem(key));
 }
-// save data to local storage
+
+
 export function setLocalStorage(key, data) {
   localStorage.setItem(key, JSON.stringify(data));
 }
-// set a listener for both touchend and click
+
+
 export function setClick(selector, callback) {
   qs(selector).addEventListener("touchend", (event) => {
     event.preventDefault();
     callback();
   });
   qs(selector).addEventListener("click", callback);
+}
+
+
+
+/**
+ * Renders a list using a template function and inserts the results into a DOM element.
+ * @param {Function} template - function that returns an HTML string for a product
+ * @param {HTMLElement} parentElement - the container to render the list into
+ * @param {Array} list - the array of items to render
+ */
+export function renderListWithTemplate(template, parentElement, list) {
+  list.forEach(item => {
+    const html = template(item);
+    parentElement.insertAdjacentHTML("beforeend", html);
+  });
 }
